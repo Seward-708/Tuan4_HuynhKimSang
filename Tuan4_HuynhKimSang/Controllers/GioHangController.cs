00b29ea2cd8180deef5e68ef34820788f0af6060
Seward <sanghuynh0803@gmail.com>
2022-03-23 10:15:31 +0700

    23/3/2022

--- Tuan4_HuynhKimSang/Controllers/GioHangController.cs
+++ Tuan4_HuynhKimSang/Controllers/GioHangController.cs
@@ -27,6 +27,7 @@ namespace Tuan4_HuynhKimSang.Controllers
         public ActionResult ThemGioHang(int id,string strURL)
         {
             List<GioHang> lstGioHang = Laygiohang();
+            var sach = db.Saches.FirstOrDefault(p => p.masach == id);
             GioHang sanpham = lstGioHang.Find(p => p.masach == id);
             if(sanpham == null)
             {
@@ -36,8 +37,16 @@ namespace Tuan4_HuynhKimSang.Controllers
             }
             else
             {
-                sanpham.iSoLuong++;
-                return Redirect(strURL);
+                if (sanpham.iSoLuong < sach.soluongton)
+                {
+                    sanpham.iSoLuong++;
+                    return Redirect(strURL);
+                }
+                else
+                {
+                    MessageBox.Show("Không có đủ sách đẻ bán");
+                    return RedirectToAction("Index", "Home");
+                }
             }
         }
 
@@ -106,10 +115,16 @@ namespace Tuan4_HuynhKimSang.Controllers
         public ActionResult Capnhatgiohang(int id, System.Web.Mvc.FormCollection collection) 
         {
             List<GioHang> lstGioHang = Laygiohang();
+            var sach = db.Saches.FirstOrDefault(p => p.masach == id);
             GioHang sanpham = lstGioHang.SingleOrDefault(p => p.masach == id);
             if(sanpham != null)
             {
                 sanpham.iSoLuong = int.Parse(collection["txtSoLg"].ToString().Trim());
+                if(sanpham.iSoLuong > sach.soluongton)
+                {
+                    MessageBox.Show("Không còn đủ sách để bán");
+                    sanpham.iSoLuong = 1;
+                }
 
             }
             return RedirectToAction("GioHang");
@@ -121,49 +136,113 @@ namespace Tuan4_HuynhKimSang.Controllers
             return RedirectToAction("GioHang");
         }
 
-        public ActionResult Dathang()
+        //public ActionResult Dathang()
+        //{
+        //    List<GioHang> lstGioHang = Laygiohang();
+        //    if (lstGioHang.Count() != 0)
+        //    {
+        //        DialogResult result = MessageBox.Show("bạn muốn đặt hàng", "Hỏi", MessageBoxButtons.OKCancel);
+        //        if (result == DialogResult.OK)
+        //        {
+        //            create invoice
+        //            DonHang invoice = new DonHang();
+        //            invoice.ngaydat = DateTime.Now;
+        //            db.DonHangs.InsertOnSubmit(invoice);
+        //            db.SubmitChanges();
+        //            int invoide_id = db.DonHangs.OrderByDescending(p => p.madon).Select(p => p.madon).FirstOrDefault();
+        //            add invoice's detail
+        //            ChiTietDonHang idetail;
+        //            foreach (var ele in lstGioHang)
+        //            {
+        //                idetail = new ChiTietDonHang();
+        //                idetail.masach = ele.masach;
+        //                idetail.madon = invoide_id;
+        //                idetail.gia = (decimal?)ele.giaban;
+        //                idetail.soluong = ele.iSoLuong;
+        //                db.ChiTietDonHangs.InsertOnSubmit(idetail);
+        //                var book = db.Saches.FirstOrDefault(p => p.masach == ele.masach);
+        //                book.soluongton -= idetail.soluong;
+        //                UpdateModel(book);
+        //            }
+        //            db.SubmitChanges();
+        //            string str = "";
+        //            int i = 1;
+        //            foreach (var ele in lstGioHang)
+        //            {
+        //                str += i + " - " + ele.tensach + "\n";
+        //                i++;
+        //            }
+        //            MessageBox.Show("Đặt hàng thành công!\n" + "---------------\n" + "Danh sách đặt hàng\n" + str);
+        //            return RedirectToAction("Index", "Home");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Giỏ hàng trống");
+        //    }
+        //    return RedirectToAction("Index", "Home");
+
+        //}
+
+
+        [HttpGet]
+        public ActionResult DatHang()
         {
+            if(Session["TaiKhoan"] == null || Session["TaiKhoan"].ToString() == "")
+            {
+                return RedirectToAction("DangNhap","NguoiDung");
+            }
+            if(Session["Giohang"] == null)
+            {
+                return RedirectToAction("Index", "Sach");
+            }
             List<GioHang> lstGioHang = Laygiohang();
-            if(lstGioHang.Count() != 0)
-            {
-                DialogResult result = MessageBox.Show("bạn muốn đặt hàng", "Hỏi", MessageBoxButtons.OKCancel);
-                if (result == DialogResult.OK)
-                {
-                    //create invoice
-                    Invoice invoice = new Invoice();
-                    invoice.Invoice_DateCreate = DateTime.Now;
-                    db.Invoices.InsertOnSubmit(invoice);
-                    db.SubmitChanges();
-                    int invoide_id = db.Invoices.OrderByDescending(p => p.Invoice_ID).Select(p => p.Invoice_ID).FirstOrDefault();
-                    //add invoice's detail
-                    Invoice_Detail idetail;
-                    foreach (var ele in lstGioHang)
-                    {
-                        idetail = new Invoice_Detail();
-                        idetail.masach = ele.masach;
-                        idetail.Invoice_ID = invoide_id;
-                        idetail.giamua = ele.giaban;
-                        idetail.soluongmua = ele.iSoLuong;
-                        db.Invoice_Details.InsertOnSubmit(idetail);
-                    }
-
-
-                    db.SubmitChanges();
-                    MessageBox.Show("Đặt hàng thành công!");
-                    return View("Index", "Home");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Giỏ hàng trống");
-            }
-            return RedirectToAction("Home");
-
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.Tongsoluongsanpham = TongSoLuongSanPham();
+            return View(lstGioHang);
         }
+        public ActionResult DatHang(System.Web.Mvc.FormCollection collection)
+        {
+            DonHang dh = new DonHang();
+            KhachHang kh = (KhachHang)Session["TaiKhoan"];
+            Sach s = new Sach();
 
+            List<GioHang> gh = Laygiohang();
+            var ngaygiao = string.Format("{0:MM/dd/yyyy}", collection["NgayGiao"]);
+
+            dh.makh = kh.makh;
+            dh.ngaydat = DateTime.Now;
+            dh.ngaygiao = DateTime.Parse(ngaygiao);
+            dh.giaohang = false;
+            dh.thanhtoan = false;
+
+            db.DonHangs.InsertOnSubmit(dh);
+            db.SubmitChanges();
+            foreach(var ele in gh)
+            {
+                ChiTietDonHang ctdh = new ChiTietDonHang();
+                ctdh.madon = dh.madon;
+                ctdh.masach = ele.masach;
+                ctdh.soluong = ele.iSoLuong;
+                ctdh.gia = (decimal)ele.giaban;
+                s = db.Saches.Single(n => n.masach == ele.masach);
+                s.soluongton -= ctdh.soluong;
+                db.SubmitChanges();
+                db.ChiTietDonHangs.InsertOnSubmit(ctdh);
+            }
+            db.SubmitChanges();
+            Session["Giohang"] = null;
+            return RedirectToAction("XacNhanDonHang", "GioHang");
+        }
 
         // GET: GioHang
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult XacNhanDonHang()
         {
             return View();
         }
